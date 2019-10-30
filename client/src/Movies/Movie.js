4c7df35b34a92b592739a85c87fd8875f4f1a680
HumphreyJ <joshhumphrey1@gmail.com>
2019-10-29 22:48:33 -0500

    Made some progress. Got stuck trying to render the Movie component.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 1;
+    const id = movie.id
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[movie.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -30,24 +30,24 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  
   return (
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>{title}</h2>
+        <h2>{movie.title}</h2>
         <div className="movie-director">
-          Director: <em>{director}</em>
+          Director: <em>{movie.director}</em>
         </div>
         <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
+          Metascore: <strong>{movie.metascore}</strong>
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {/* {movie.stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
-        ))}
+        ))} */}
       </div>
       <div className="save-button">Save</div>
     </div>
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -28,24 +29,26 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
+    <Link to={`/movies/${movie.id}`}>
     <div className="movie-card">
-      <h2>{title}</h2>
+      <h2>{movie.title}</h2>
       <div className="movie-director">
-        Director: <em>{director}</em>
+        Director: <em>{movie.director}</em>
       </div>
       <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
+        Metascore: <strong>{movie.metascore}</strong>
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {movie.stars.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
       ))}
     </div>
+    </Link>
   );
 }
 
